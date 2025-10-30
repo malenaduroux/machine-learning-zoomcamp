3489d9c1b3b46d2a31f0f6048fa7d5f4d636ed00
malenaduroux <142320045+malenaduroux@users.noreply.github.com>
2025-10-30 21:57:34 +0000

    homework 05

---
+++ wk05-homework/predict.py
@@ -0,0 +1,34 @@
+import pickle
+
+from fastapi import FastAPI
+import uvicorn
+
+from typing import Dict, Any
+
+client = {
+    "lead_source": "paid_ads",
+    "number_of_courses_viewed": 2,
+    "annual_income": 79276.0
+}
+
+
+with open('pipeline_v2.bin', 'rb') as f_in:
+    pipeline = pickle.load(f_in)
+
+code = FastAPI(title="lead-scoring-prediction")
+
+def predict_single(client):
+    result = pipeline.predict_proba(client)[0, 1]
+    return float(result)
+
+@code.post("/predict")
+def predict(client: Dict[str, Any]):
+    prob = predict_single(client)
+
+    return {
+        "Conversion Probability": prob,
+        "Converted?": bool(prob >= 0.5)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(code, host="0.0.0.0", port=9696)
---
+++ wk05-homework/test.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "http://localhost:9696/predict"
+
+client = {
+    "lead_source": "organic_search",
+    "number_of_courses_viewed": 4,
+    "annual_income": 80304.0
+}
+
+response = requests.post(url, json=client).json()
+
+print(response)
+if response['Converted?']:
+    print('client is likely to convert, target with offer')
+else:
+    print('client is not likely to convert')
